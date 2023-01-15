45ef4891d8315d1e30b3c8acb5df9e8752af9169
jmlii <44909874+jmlii@users.noreply.github.com>
2023-01-15 20:37:22 +0200

    Osa 6 redux-anecdotes tehtävät 6.13-6.14

--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
-    notification: notificationReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 })
 
